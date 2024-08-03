8de71fca09a951a1145e22303164c4261e95976a
gdiazq <116692302+gdiazq@users.noreply.github.com>
2024-08-03 02:44:11 -0400

    Update LoginButton.tsx

--- src/components/ui/auth/LoginButton.tsx
+++ src/components/ui/auth/LoginButton.tsx
@@ -7,7 +7,7 @@ export const LoginButton = () => {
 
     return (
         <div className="mt-4">
-            <Button type="submit" className="text-base bg-white cursor-pointer">
+            <Button type="submit" className="text-base bg-white text-black cursor-pointer">
                 Login
             </Button>
         </div>
