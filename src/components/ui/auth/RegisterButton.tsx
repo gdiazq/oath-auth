f70467687143428347f9ce63756946ca2ca5939a
gdiazq <116692302+gdiazq@users.noreply.github.com>
2024-08-03 02:44:32 -0400

    Update RegisterButton.tsx

--- src/components/ui/auth/RegisterButton.tsx
+++ src/components/ui/auth/RegisterButton.tsx
@@ -7,7 +7,7 @@ export const RegisterButton = () => {
 
     return (
         <div className="mt-4">
-            <Button type="submit" className="text-base bg-white cursor-pointer">
+            <Button type="submit" className="text-base bg-white text-black cursor-pointer">
                 Create an account
             </Button>
         </div>
