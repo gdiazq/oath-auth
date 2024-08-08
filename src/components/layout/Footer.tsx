1a8b4856bbb501faa73d7be2931de51c798487b1
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-08-07 23:41:26 -0400

    Arreglando el footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,16 +4,16 @@ const currentYear = new Date().getFullYear();
 
 export const Footer = () => {
     return (
-        <footer className="mt-16 mx-auto mb-5 flex flex-row justify-between">
+        <footer className="mt-16 mx-auto mb-5 flex flex-row justify-between content-end">
             <ul className="flex flex-row justify-between flex-wrap items-center mt-3 text-sm font-medium dark:text-white/90 sm:mt-0 gap-x-16 lg:gap-x-20">
                 <li>
-                    <p className="text-sm lg:text-base text-white"> {currentYear} Guillermo Diaz </p>
+                    <p className="text-sm lg:text-base text-black dark:text-white"> {currentYear} Guillermo Diaz </p>
                 </li>
                 <li>
-                    <a href='#inicio' className="text-sm lg:text-base text-white">Inicio</a>
+                    <a href='#inicio' className="text-sm lg:text-base text-black dark:text-white">Inicio</a>
                 </li>
                 <li>
-                    <a href="mailto:guillermoe.diazq@gmail.com" target="_blank" rel="noopener noreferrer" className="text-sm lg:text-base text-white">Contactame</a>
+                    <a href="mailto:guillermoe.diazq@gmail.com" target="_blank" rel="noopener noreferrer" className="text-sm lg:text-base text-black dark:text-white">Contactame</a>
                 </li>
             </ul>
         </footer>
