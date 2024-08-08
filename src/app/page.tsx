5497f8f6e4d2dba255da8ddac741a0f5e2f90fcf
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-08-07 23:25:51 -0400

    Se agrega el Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/layout/Header";
+import { Footer } from '@/components/layout/Footer';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
             </h1>
           </div>
         </main>
+      <Footer />
     </>
   );
 }
