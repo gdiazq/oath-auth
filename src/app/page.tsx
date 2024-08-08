b0b2eb314e6fb4d732355461217cc54307e26c51
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-08-07 23:50:54 -0400

    Terminado por ahora

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 import Header from "@/components/layout/Header";
-import { Footer } from '@/components/layout/Footer';
 
 export default function Home() {
   return (
     <>
       <Header />
-        <main className="p-4">
-          <div className="space-y-6 py-24 text-center px-4">
-            <h1 className="text-5xl font-semibold text-black dark:text-white drop-shadow-md">
-              Authentication System with OAuth and NextAuth.js
-            </h1>
-          </div>
-        </main>
-      <Footer />
+      <main className="p-4">
+        <div className="space-y-6 py-24 text-center px-4">
+          <h1 className="text-5xl font-semibold text-black dark:text-white drop-shadow-md">
+            Authentication System with OAuth and NextAuth.js
+          </h1>
+        </div>
+      </main>
     </>
   );
 }
