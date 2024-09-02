f2faedc69a4b7171397d6a942d4b3356134b3652
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-09-02 16:06:22 -0400

    SiguiendoV2

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -5,9 +5,6 @@ import { AuthError } from 'next-auth';
 import { signIn } from '@/auth'
 import { LoginHotelSchema } from '@/schemas/signin';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
-import { generateVerificationToken } from '@/lib/tokens';
-import { getUserbyEmail } from '@/data/user';
-import { sendVerificationEmail } from '@/lib/mail';
 
 export const login = async (values: z.infer<typeof LoginHotelSchema>) => {
     const validatedFields = LoginHotelSchema.safeParse(values);
@@ -16,17 +13,6 @@ export const login = async (values: z.infer<typeof LoginHotelSchema>) => {
     }
     
     const { email, password } = validatedFields.data;
-
-    const existingUser = await getUserbyEmail(email);
-    if (!existingUser || !existingUser.email || !existingUser.password) {
-        return { error: "Email doe not exist" };
-    }
-
-    if (!existingUser.emailVerified) {
-        const verificationToken = await generateVerificationToken(existingUser.email);
-        await sendVerificationEmail(verificationToken.email, verificationToken.token);
-        return { error: "Confirmation email sent!"  };
-    }
 
     try {
         await signIn("credentials", {
