161682f71a36903580da504945704737fbe29792
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-09-03 01:03:48 -0400

    Esta OK, tengo que subir el readme ahora

--- src/components/ui/auth/LoginButton.tsx
+++ src/components/ui/auth/LoginButton.tsx
@@ -2,12 +2,19 @@
 
 import React from "react";
 import { Button } from "@nextui-org/react";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const LoginButton = () => {
+    const onClick = (provider: "credentials") => {
+        signIn(provider, {
+            callbackUrl: DEFAULT_LOGIN_REDIRECT,
+        })
+    }
 
     return (
         <div className="mt-4">
-            <Button type="submit" className="text-base bg-white text-black cursor-pointer">
+            <Button type="submit" className="text-base bg-white text-black cursor-pointer" onClick={() => onClick("credentials")}>
                 Login
             </Button>
         </div>
